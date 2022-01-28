e78015d9ca7d9d6a23894b4768ca1a3893481b68
BarbonFisso <barbieri.mat@hotmail.it>
2022-01-28 17:46:56 +0100

    Add: ComicSeeder

---
+++ database/seeds/ComicSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Comic;
+use Illuminate\Database\Seeder;
+
+class ComicSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Comic::class, 50) -> create();
+    }
+}
