c098b0d591faba7f9dd54f69c957154310a4f333
BarbonFisso <barbieri.mat@hotmail.it>
2022-01-28 17:45:43 +0100

    Add: ComicFactory

---
+++ database/factories/ComicFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Comic;
+use Faker\Generator as Faker;
+
+$factory->define(Comic::class, function (Faker $faker) {
+    return [
+
+        'title' => $faker -> words(3, true),
+        'author' => $faker -> words(5, true),
+        'release_date' => $faker -> date(),
+        'pages' => $faker -> numberBetween(50,500)
+    ];
+});
