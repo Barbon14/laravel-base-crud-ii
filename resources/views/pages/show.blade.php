dfb60c385370805851725a04c45b162d99c0f74b
BarbonFisso <barbieri.mat@hotmail.it>
2022-01-28 17:34:43 +0100

    Add: show page

---
+++ resources/views/pages/show.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.main-layout')
+
+@section('content')
+<section>
+    <h2>
+        {{ $comic->title }}
+    </h2>
+    <h3>
+        {{ $comic->subtitle }}
+    </h3>
+    <h4>
+        {{ $comic->relase_date }}
+    </h4>
+    <h4>
+        Numero di pagine: {{ $comic->pages }}
+    </h4>
+</section>
+@endsection
